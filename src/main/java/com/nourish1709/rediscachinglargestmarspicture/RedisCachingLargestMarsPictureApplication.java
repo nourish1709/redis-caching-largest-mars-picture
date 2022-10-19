81e4ea6c9c83f7f735859bd7466f9dc2156e397c
Yevhenii Savonenko <yevs@jacobianengineering.com>
2022-10-19 08:38:39 +0300

    Initial commit

---
+++ src/main/java/com/nourish1709/rediscachinglargestmarspicture/RedisCachingLargestMarsPictureApplication.java
@@ -0,0 +1,13 @@
+package com.nourish1709.rediscachinglargestmarspicture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCachingLargestMarsPictureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisCachingLargestMarsPictureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nourish1709/rediscachinglargestmarspicture/RedisCachingLargestMarsPictureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nourish1709.rediscachinglargestmarspicture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisCachingLargestMarsPictureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
